6dd9a704f85bdf84c2778abafe46fd73a02981a3
Benson <bhain7@gmail.com>
2020-09-02 20:11:12 -0500

    Fixed Story Progression
    It works!

--- MainFile.py
+++ MainFile.py
@@ -2,14 +2,28 @@ gameison = True
 
 #Class to hold the stages
 class stage:
-     def __init__(self, prompt, option1, option2, option3):
+     def __init__(self, prompt):
          self.prompt = prompt
-         self.option1 = option1
-         self.option2 = option2
-         self.option3 = option3
+         
+         self.option1 = ""
+         self.option2 = ""
+         self.option3 = ""
 
-gamestart = stage("Welcome to the game! (write the first prompt here for users )\n","stage1","stage2","stage3")
+#Initiate all the stages
+gamestart = stage("Welcome to the game! (write the first prompt here for users )\n Option1: go to Paris \n")
+paris = stage("Bonjour, you arrive in Paris and die of dyssentary. \n")
+
+
+
+#Set the stage paths
+gamestart.option1 = (paris)
+
+
+
+
 currentstage = gamestart
+
+
 
 def promptresponse():
     global resp
@@ -25,4 +39,19 @@ while gameison == True:
         print("Thanks for playing!")
         gameison = False
         
+    if resp == "1":
+        currentstage = currentstage.option1
+    elif resp == "2":
+        currentstage = currentstage.option2
+    elif resp == "3":
+        currentstage = currentstage.option3
+    else:
+        print("Sorry! Bad input! Type 1 2 or 3! Thanks buddy!")
+        
+
+
     
+
+
+
+
