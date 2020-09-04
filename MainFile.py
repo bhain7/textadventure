786bb8add1b81a7adcca874aa7e879a842ef4919
Louisianabob <70178826+Louisianabob@users.noreply.github.com>
2020-09-03 20:40:45 -0500

    Robert - New stages

--- MainFile.py
+++ MainFile.py
@@ -10,15 +10,19 @@ class stage:
          self.option3 = ""
 
 #Initiate all the stages
-gamestart = stage("Welcome to the game! (write the first prompt here for users )\n Option1: go to Paris \n")
-paris = stage("Bonjour, you arrive in Paris and die of dyssentary. \n")
-
+gamestart = stage("Welcome to the game! (write the first prompt here for users )\n Option1: go to Paris \n Option2: Guess who dropped their car keys in the Sewers. It was you.  \n Option3: Give up and be at your home. This is MTV and u will be at ur at ur crib \n")
+paris = stage("Bonjour, you arrive in Paris and die of dyssentary. \n You could go somewhere though? \n")
+Sewers = stage("Dripping noises and farty smells. You're not in Paris though! \n")
+Home = stage("You're home because of quarantine \n")
 
 
 #Set the stage paths
-gamestart.option1 = (paris)
+gamestart.option1 = paris
+gamestart.option2 = Sewers
+gamestart.option3 = Home
 
-
+paris.option1 = Sewers
+paris.option2 = Home
 
 
 currentstage = gamestart
